9309e46f5e057ee0bd22b40e662ff3b5abe50f01
Tanaji-25 <sumitjadhav0725@gmail.com>
2024-09-28 21:58:21 +0530

    previous

--- myServer.js
+++ myServer.js
@@ -379,7 +379,6 @@ app.post("/submit-date",(req,res)=>{
 
 
 
-app.listen(3001, () => {
-  console.log('Server is running on port 3001');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
