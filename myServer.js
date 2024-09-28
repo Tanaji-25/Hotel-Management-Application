81f1ba0180bb19856e87d8cc48d60204d47dd335
Tanaji-25 <sumitjadhav0725@gmail.com>
2024-09-28 22:09:04 +0530

    change port

--- myServer.js
+++ myServer.js
@@ -12,6 +12,7 @@ const connectDB = require('./public/conn');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const HOST = '0.0.0.0';
 connectDB();
 const static_path = path.join(__dirname, './public/src');
 const storage = multer.memoryStorage();
@@ -379,6 +380,6 @@ app.post("/submit-date",(req,res)=>{
 
 
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on ${HOST}:${PORT}`);
 });
