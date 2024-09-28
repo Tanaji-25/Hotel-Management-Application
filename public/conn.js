57e72c24ab839debb24890f0bb0d5334ab726896
Tanaji-25 <143085941+Tanaji-25@users.noreply.github.com>
2024-09-28 22:37:35 +0530

    Update conn.js

--- public/conn.js
+++ public/conn.js
@@ -2,7 +2,7 @@ const mongoose= require('mongoose');
 
 async function connectDB(){
     try {
-       await mongoose.connect('mongodb://localhost:27017/HotelManagement');
+       await mongoose.connect('mongodb+srv://TanajiJadhav2625:sumit%402625@cluster0.esltb.mongodb.net/hotelmanagement');
         console.log('Connected')
     } catch (error) {
         console.log('error',error)
